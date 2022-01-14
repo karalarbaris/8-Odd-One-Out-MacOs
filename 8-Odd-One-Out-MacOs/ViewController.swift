3bad47e8c204676a2151b8f152e029c0ca12cee7
Baris Karalar <karalarbaris@gmail.com>
2022-01-14 09:16:58 +0100

    Initial Commit

---
+++ 8-Odd-One-Out-MacOs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  8-Odd-One-Out-MacOs
+//
+//  Created by Baris Karalar on 14.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
